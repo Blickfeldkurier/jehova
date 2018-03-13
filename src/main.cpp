0d3371e681689de508ee070310c04fbb3d28118d
Blickfeldkurier <Blickfeldkurier@posteo.de>
2018-03-13 11:07:56 +0100

    towards getopt

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <getopt.h>
 #include "canvas.h"
 #include "network.h"
 
+void printHelp(){
+    std::cout <<
+        "--help\tShow this help\n" <<
+        "--fullscreen\tRun in Fullscreen";
+    exit(1);
+}
+
 int main(int argc, char*argv[]){
+    const char* const short_opts = "fh?";
+    int optionIndex;
+    const option long_opts[] = {
+        {"help", no_argument, nullptr, 'h'},
+        {nullptr, no_argument, nullptr, 0}
+    };
+
+    while(int opt = getopt_long(argc, argv, short_opts, nullptr, nullptr) != -1){
+      switch(opt){
+        case 'f':{
+        std::cout << "FULLSCREEN\n";
+      }break;
+        case 'h':
+        case '?':{
+            printHelp();
+        }break;
+      }
+    }
+
     Canvas *canvas = new Canvas();
     Network *network = new Network();
 
